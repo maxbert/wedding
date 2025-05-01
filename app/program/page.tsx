4fd97207af220cb99667c178b1970b46fab40e1b
Max Bertfield <max@bertfield.com>
2025-05-01 01:24:30 -0400

    fixes

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -1,42 +1,29 @@
+'use client';
+
 import Image from "next/image";
 import { Navigation } from "@/components/Navigation";
-import { DecorativeLeaf } from "@/components/DecorativeElements";
-import { DecorativeFlower } from "@/components/DecorativeElements";
+import { DecorativeLeaf, DecorativeFlower, CenterFlower } from "@/components/DecorativeElements";
 
 export default function ProgramPage() {
   return (
     <div className="min-h-screen bg-[#f5e6d3]">
       <Navigation currentPage="program"/>
-
-      {/* Main container with border */}
-      <div className="relative h-[calc(100vh-3.5rem)]">
-        {/* Screen edge borders */}
-        <div className="absolute inset-8 border-[3px] border-black rounded-[2.5rem]">
-          <div className="absolute inset-3 border-[3px] border-black rounded-[2rem]">
-            {/* Center content */}
-            <div className="absolute inset-0 flex items-center justify-center">
-              {/* Content card */}
-              <div className="bg-[#fff5eb] rounded-[2rem] w-[32rem] py-16 px-12 relative">
-                {/* Decorative top element */}
-                <div className="absolute top-2 left-1/2 -translate-x-1/2">
-                  <Image
-                    src="/images/center-flowe.png"
-                    alt="Decorative element"
-                    width={40}
-                    height={40}
-                    className="opacity-80"
-                  />
-                </div>
+      <div className="h-[calc(100vh-3.5rem)] relative px-2 md:px-8">
+        <div className="h-full border-[3px] border-black rounded-[2.5rem] p-3">
+          <div className="h-full border-[3px] border-black rounded-[2rem]">
+            <div className="h-full flex items-center justify-center">
+              <div className="bg-[#fff5eb] rounded-[2rem] w-[32rem] max-w-full py-16 px-6 md:px-12 relative">
+                <CenterFlower />
 
                 {/* Date */}
                 <div className="text-center mb-4 p-4">
                   <h1 className="font-['sloop'] text-5xl">August 24, 2025</h1>
-                  <div className="w-96 h-[1px] bg-black mx-auto mt-[-10px]"></div>
+                  <div className="w-full max-w-[24rem] mx-auto h-[1px] bg-black mt-[-10px]"></div>
                 </div>
 
                 {/* Timeline */}
                 <div className="w-full items-center mx-auto">
-                  <div className="">
+                  <div className="space-y-2">
                     <div className="flex gap-8">
                       <p className="w-24 text-right">3:30pm</p>
                       <p>Guests Arrive</p>
@@ -64,7 +51,6 @@ export default function ProgramPage() {
                   </div>
                 </div>
 
-                {/* Decorative bottom element */}
                 <div className="absolute -bottom-8 left-1/2 -translate-x-1/2">
                   <div className="w-4 h-4 rotate-45 border border-black"></div>
                 </div>
@@ -72,7 +58,6 @@ export default function ProgramPage() {
             </div>
           </div>
         </div>
-
         <DecorativeLeaf />
         <DecorativeFlower />
       </div>
